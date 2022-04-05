d7631454968bee6d8dbfd8756f34373b184a7b1f
Anton <ya.antonio19@gmail.com>
2022-04-06 01:58:23 +0300

    Initial commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ZaitsevBankAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger<UsersController> _logger;
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+        //https://localhost:7008/api/Users/Get
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            await Task.Delay(100);
+            return "connect";
+        }
+
+        [HttpGet]
+        public async Task<string> GetHelp(int id)
+        {
+            await Task.Delay(100);
+            return "connect + "  + id.ToString() ;
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ZaitsevBankAPI
+ 
